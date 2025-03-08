ba35e6a0a9a49d04fb561fea6a841c5e83b0e7e6
MadeeshaSK <madeeshasachindu2@gmail.com>
2025-03-08 09:25:39 +0530

    fs, os, http co modules, npm dotenv, npm nodemon, postman software,

--- Day-2/app.js
+++ Day-2/app.js
@@ -132,3 +132,87 @@ console.log('Tmpdir', os.tmpdir());
 // web client request from API server (using node), it response
 
 // http module
+const http = require('http');
+require('dotenv').config(); // to access env use dotenv from install npm install dotenv
+const PORT = process.env.PORT; // best practice use env , dont use const PORT = 3000;
+const server = http.createServer((req, res) => {
+  //GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD, CONNECT, TRACE
+  // load data from server(GET)
+  if (req.method === 'GET' && req.url === '/link') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end("Read data from server");
+// we can use that way GET method, but can not use POST, PUT, DELETE, PATCH, OPTIONS, HEAD, CONNECT, TRACE
+// for that we use Postman software
+// https://www.postman.com/downloads/
+// option is the pre request, we can use that way same as GET method to check whether it is working or not
+
+
+  // give data to server(POST)
+  } else if (req.method === 'POST' && req.url === '/link2') {
+    res.writeHead(201, { 'Content-Type': 'text/plain' });
+    res.end("Create data to server");
+
+  // update data to server(PUT)
+  } else if (req.method === 'PUT' && req.url === '/link3') {
+    res.writeHead(202, { 'Content-Type': 'text/plain' });
+    res.end("Update data to server");
+
+  // delete data from server(DELETE)
+  } else if (req.method === 'DELETE' && req.url === '/link4') {
+    res.writeHead(204, { 'Content-Type': 'text/plain' });
+    res.end("Delete data from server");
+
+  }else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end("Route Not Found");
+  }
+});
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  // access project : http://localhost:3000/link
+});
+// to access env use dotenv from install npm install dotenv
+//  then come files node_modules(dotenv is in this file),
+//  package.json (dependencies of all node_module and recod), package-lock.json(log of package json), .env
+/*
+dont delete package json and package-lock.json, if delete node_modules, then run npm install
+then it read dependencies from package.json and install all dependencies
+*/
+/*
+status codes:
+200 - OK
+201 - Created
+202 - Accepted
+400 - Bad Request
+401 - Unauthorized
+403 - Forbidden
+404 - Not Found
+405 - Method Not Allowed
+500 - Internal Server Error
+501 - Not Implemented
+502 - Bad Gateway
+503 - Service Unavailable
+504 - Gateway Timeout
+...
+100-199 - Informational
+200-299 - Success
+300-399 - Redirection
+400-499 - Client Error
+500-599 - Server Error
+
+now type http://localhost:3000/link in browser
+output: Read data from server
+if link is wrong like http://localhost:3000/lin
+output: Route Not Found
+*/
+
+/*
+Nodemon
+this is a tool that helps develop node.js based applications by automatically restarting
+ the node application when file changes in the directory are detected.
+ otherwise we have to stop the server and run again
+npm install -g nodemon
+nodemon app.js
+this can only use in development mode, not in production mode
+
+*/
